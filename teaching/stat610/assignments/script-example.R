bbce3428e222f3a52cc31f4ed646ce3951404ce0
Julia Fukuyama <julia.fukuyama@gmail.com>
2020-08-30 23:00:28 -0400

    homework 1

---
+++ teaching/stat610/assignments/script-example.R
@@ -0,0 +1,7 @@
+## An example R script, can be invoked using Rscript script-example.R in the terminal
+
+x = 1+1
+print("You can use the print or cat function to print out information.")
+cat("Anything that would normally be printed in an interactive R session will be printed to the console.")
+paste("The value of x is: ", x, sep = "")
+x
